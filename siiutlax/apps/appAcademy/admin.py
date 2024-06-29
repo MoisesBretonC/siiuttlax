20e228085faca57a095ee8143558d370d987b5d3
DeividDDHXT <jestmon02@gmail.com>
2024-06-28 21:30:41 -0600

    prueba

--- siiutlax/apps/appAcademy/admin.py
+++ siiutlax/apps/appAcademy/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Professor, Students
 
 # Register your models here.
+
+admin.site.register(Category)
+# admin.site.register(Professor)
+# admin.site.register(Student)
+
+
+@admin.register(Professor)
+class ProfesorAdmin(admin.ModelAdmin):
+    fields = ["username", "email", "first_name", "category", "title", "numero_trabajador"]
+
+
+@admin.register(Students)
+class StudentAdmin(admin.ModelAdmin):
+    fields = ["username", "email", "first_name", "matricula"]
--- siiutlax/apps/appAcademy/models.py
+++ siiutlax/apps/appAcademy/models.py
@@ -9,17 +9,31 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     short_name = models.CharField(max_length=15)
     description = models.TextField()
-
+    
+    def __str__(self):
+        return self.name
+         
+    class Meta:
+            verbose_name = "Categoria"
+            verbose_name_plural = "Categorias"
 
 class Professor(User):
-    numero_trabajador = models.IntegerField(9)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    numero_trabajador = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=100, null=True)
+    
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        verbose_name = "Profesor"
+        verbose_name_plural = "Profesores"
 
 
-class Students(User):
-    matricula = models.CharField(max_length=12)    
-
-
-class Principal(User):
-    numero_trabajador = models.IntegerField(12) 
+class Student(User):
+    matricula = models.CharField(max_length=12)
+    def __str__(self):
+        return self.username
+    class Meta:
+        verbose_name = "Estudiante"
+        verbose_name_plural = "Estudiantes"
