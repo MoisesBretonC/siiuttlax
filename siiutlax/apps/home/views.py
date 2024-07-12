10424a8fc961063de0da04ca397b633da09a6a95
Moises <moisesbreton11@gmail.com>
2024-07-12 11:58:46 -0600

    autenticacion

--- siiutlax/apps/home/views.py
+++ siiutlax/apps/home/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
+@login_required
 def home(request):
-    return render(request, 'home/home.html')
+    user = request.user
+    try:
+        if user.professor: type_user = 'professor'
+    except: type_user = 'other'
+
+    if type_user == 'other':
+        try:
+            if user.student: type_user = 'student'
+        except: type_user = 'other'
+        
+    context = {
+        "user": user,
+        "type_user": type_user
+    }
+    return render(request, 'home/home.html', context)
 
 def welcome_view(request):
     return render(request, 'welcome/welcome.html')
