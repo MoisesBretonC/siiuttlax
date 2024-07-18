e03e1572528994b4d079b0e5923c44139ff896c6
Luis Javier <luisjavierhernandez150@gmail.com>
2024-07-18 13:32:48 -0600

    YaEstaChidori

--- siiutlax/apps/appAcademy/urls.py
+++ siiutlax/apps/appAcademy/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 
 from . import views
 
-app_name = "academy"
+app_name = 'academy'
 urlpatterns = [
-    path('', views.academy, name='academy'),
+    path('student/create',
+        views.create_student,
+        name='create_student'),
+    path('profesor/create',
+         views.create_professor,
+         name='create_profesor'),
 ]
+
