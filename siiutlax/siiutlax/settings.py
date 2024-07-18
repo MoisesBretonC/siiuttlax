d20d718e5a6f692b5d6cbe6b6a1a16226f4add7e
Luis Javier <luisjavierhernandez150@gmail.com>
2024-07-18 12:48:31 -0600

    Correccion2

--- siiutlax/siiutlax/settings.py
+++ siiutlax/siiutlax/settings.py
@@ -12,9 +12,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from django.conf.global_settings import LOGIN_REDIRECT_URL
 
-load_dotenv()
-
-
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
